a951395ad1d90e3f180811633a7a6e19cd4a60a3
notpluto <srijan.kpr@gmail.com>
2019-02-21 14:45:13 +0530

    add case for empty value

--- day_9/index.js
+++ day_9/index.js
@@ -1,27 +1,31 @@
-var tagArray = [{name: "hello"}];
+// var tagArray = [{name: "hello"}];
 
+const input = document.body.querySelector('input')
 const tag = document.querySelector('.tag');
 const tagText = document.querySelector('.tag-text');
-const button = document.querySelector('.btn');
-// const cancel = document.querySelector('.delete');
 const test = document.querySelector('.test');
 
-document.body.querySelector('input').addEventListener('keyup', function(e){
-if (e.keyCode==13) {
-		const button = document.createElement('button'); 
-		button.classList.add('btn');
-    button.innerText= tagText.value;
+// To create a tag add it to the div element
+function addTag(e){
+    if (tagText.value === '') {
+        return alert("Enter text to create a tag");
+    }
+    else if (e.keyCode==13) {
+        const button = document.createElement('button'); 
+        button.classList.add('btn');
+        button.innerText= tagText.value;
 
-    const cross = document.createElement('span');
-    cross.classList.add('delete');
-    cross.innerText = " x";
+        const cross = document.createElement('span');
+        cross.classList.add('delete');
+        cross.innerText = " x";
 
-    button.appendChild(cross);
-    test.appendChild(button);
-    tagText.value='';
-}
-});
+        button.appendChild(cross);
+        test.appendChild(button);
+        tagText.value='';
+    }
+};
 
+// To remove the div element containing the tag
 function remove(e) {
 	if (e.target.classList.contains('delete')) {
       test.removeChild(e.target.parentNode);
@@ -29,6 +33,8 @@ function remove(e) {
     }
 }
 
+// Event Listeners
+input.addEventListener('keyup', addTag)
 tag.addEventListener('click', remove)
 
 
