43b25b8838e076bb9f6a1411c9dd93baf5ce7b89
notpluto <srijan.kpr@gmail.com>
2019-03-06 21:24:40 +0530

    add draggable

--- day_12/main.js
+++ day_12/main.js
@@ -4,15 +4,17 @@ const input = document.body.querySelector('input')
 const inputText = document.body.querySelector('.input');
 const ul = document.body.querySelector('ul');
 const plus = document.body.querySelector('.plus');
+const items = document.body.querySelectorAll('.items');
 
 // function to add items
 function addItem(e){
 	if (inputText.value === '') {
         return false;
       }
-	else if (e.target.classList.contains('plus')) {
+	else if (e.target.classList.contains('plus'))  {
 		const li = document.createElement('li')
 		li.classList.add('items');
+		li.setAttribute("draggable", true);
 		li.innerText = inputText.value;
 
 		ul.appendChild(li);
@@ -20,8 +22,15 @@ function addItem(e){
 	}
 };
 
+// function DragStart(e) {
+//   e.target.style.opacity = '0.4';  // e.target is the source node.
+//   console.log('called');
+// }
+// items.forEach(items => item.addEventListener('dragstart', DragStart));
+
+
 // Event Listeners
-plus.addEventListener('click', addItem)
+plus.addEventListener('click', addItem);
 
 
 
