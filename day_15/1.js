39e2f87f7e322e45cf2c708eb36419c3d183b759
notpluto <srijan.kpr@gmail.com>
2019-03-14 01:00:51 +0530

    add exercise

--- day_15/1.js
+++ day_15/1.js
@@ -1,42 +1,42 @@
 // The Car and the Truck have similar behavior and properties.
 // Rewrite them in a way such that they share those properties.
 
-  var Vehicle = function(driver) {
-    this.driver = driver;
-    this.speed = 0;
-    this.drive = function(mph) {
-      this.speed = mph;
-      return this.driver + ' driving at ' + mph + ' miles per hour';
-    };
-  };
+  // var Vehicle = function(driver) {
+  //   this.driver = driver;
+  //   this.speed = 0;
+  //   this.drive = function(mph) {
+  //     this.speed = mph;
+  //     return this.driver + ' driving at ' + mph + ' miles per hour';
+  //   };
+  // };
 
-  var Car = function(driver) {
-    this.driver = driver;
-    this.speed = 0;
-    this.drive = function(mph) {
-      this.speed = mph;
-      return this.driver + ' driving at ' + mph + ' miles per hour';
-    };
-  };
+  // var Car = function(driver) {
+  //   this.driver = driver;
+  //   this.speed = 0;
+  //   this.drive = function(mph) {
+  //     this.speed = mph;
+  //     return this.driver + ' driving at ' + mph + ' miles per hour';
+  //   };
+  // };
 
-  var Truck = function(driver) {
-    this.driver = driver;
-    this.speed = 0;
-    this.cargo = [];
-    this.drive = function(mph) {
-      this.speed = mph;
-      return this.driver + ' driving at ' + mph + ' miles per hour';
-    };
+  // var Truck = function(driver) {
+  //   this.driver = driver;
+  //   this.speed = 0;
+  //   this.cargo = [];
+  //   this.drive = function(mph) {
+  //     this.speed = mph;
+  //     return this.driver + ' driving at ' + mph + ' miles per hour';
+  //   };
 
-    this.loadCargo = function(cargo) {
-      this.cargo.push(cargo);
-      return this;
-    };
+  //   this.loadCargo = function(cargo) {
+  //     this.cargo.push(cargo);
+  //     return this;
+  //   };
 
-    this.unloadCargo = function() {
-      return this.cargo.pop();
-    };
-  };
+  //   this.unloadCargo = function() {
+  //     return this.cargo.pop();
+  //   };
+  // };
 
 
 // re-written function
@@ -65,5 +65,8 @@ console.log(Car.drive(34))
 // Adam driving at 34 miles per hour
 
 console.log(Truck.loadCargo(13))
+
+
+// Create an object called Teacher derived from the Person class, and implement a method called teach which receives a string called subject, and prints out.
 
 
