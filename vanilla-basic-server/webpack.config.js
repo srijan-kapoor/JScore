df11ab6e7028b344fdbae255aa6e50f045893b30
notpluto <srijan.kpr@gmail.com>
2019-03-14 16:14:34 +0530

    init setup

---
+++ vanilla-basic-server/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const Html = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './assets/js/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader','css-loader','sass-loader']
+			}
+		]
+	},
+	devServer: {
+		port: 8000,
+		contentBase: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [new Html({
+		template: './index.html',
+		title: "Main"
+	})]
+}
